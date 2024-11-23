a3972dcaae367b6fc3daefa37a6e4feb2c08173d
Lowie Verschelden <lowie@lowieverschelden.be>
2024-11-23 19:22:31 +0100

    parte Lowie

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log('Hola Dana');
+const btnfetchJoke = document.getElementById('fetchJoke');
+const ulElement = document.getElementById('jokeList');
+
+function obtenerChiste() {
+    fetch(('https://api.chucknorris.io/jokes/random')).then((response) => {
+        if (!response.ok) {
+            throw 'Solicitud sin exito'
+        }
+        return response.json;
+    })
+
+    .then ((chiste) => {
+        console.log(chiste)
+        renderizarChiste(chiste);
+    })
+}
+
+btnfetchJoke.addEventListener('click',function() {
+    obtenerChiste();
+})
+
