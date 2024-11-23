53a00f992d146ae32b6e6fb15df3a47a5d50c888
dianat-prog <dianatprogramadora@gmail.com>
2024-11-23 19:37:55 +0100

    añado función renderizarChiste

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ const btnfetchJoke = document.getElementById('fetchJoke');
 const ulElement = document.getElementById('jokeList');
 
 function obtenerChiste() {
-    fetch(('https://api.chucknorris.io/jokes/random')).then((response) => {
+    fetch('https://api.chucknorris.io/jokes/random').then((response) => {
         if (!response.ok) {
             throw 'Solicitud sin exito'
         }
-        return response.json;
+        return response.json();
     })
 
     .then ((chiste) => {
@@ -19,3 +19,35 @@ btnfetchJoke.addEventListener('click',function() {
     obtenerChiste();
 })
 
+function renderizarChiste(chiste){
+
+    const {id,value,icon_url}=chiste;
+    const copiaChiste ={...chiste}
+
+    const liElemnent=document.createElement('li');
+    //liElemnent.classList.add('liElemnent')
+    liElemnent.id=id;
+
+    const divContainer=document.createElement('div');
+    divContainer.classList.add('divContainer');
+
+    const parrafo=document.createElement('p');
+    parrafo.textContent=value;
+
+    const btnEliminar=document.createElement('button');
+    btnEliminar.id=id;
+    btnEliminar.textContent='Eliminar';
+    btnEliminar.addEventListener('click',function(){
+        localStorage.removeItem(btnEliminar.id);
+        const liRemove =document.getElementById(btnEliminar.id); 
+        if (liRemove){
+            liRemove.remove();
+        }
+    })
+
+    divContainer.appendChild(parrafo)
+    divContainer.appendChild(btnEliminar)
+    liElemnent.appendChild(divContainer)
+    ulElement.appendChild(liElemnent)
+    guardarLocalstore(id,copiaChiste)
+}
